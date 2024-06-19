6ab37d78a597da644103384cbebedb7624986fd4
Jonas Torriero-Meskour <jonas.torriero-meskour@ens-lyon.fr>
2024-06-19 16:23:46 +0200

    Add representation to proof for the case implying the use of a tensor

--- code.ml
+++ code.ml
@@ -43,37 +43,45 @@ let aux_unpar seq i =
   aux_par1 seq i
 
 let aux_tensor seq1 seq2 =
-match List.rev seq1, seq2 with
-| x::xs, y::ys -> (List.rev xs) @ (T(x, y)::ys)
-| _, _ -> failwith "Bad construction"
+  match List.rev seq1, seq2 with
+  | x::xs, y::ys -> (List.rev xs) @ (T(x, y)::ys)
+  | _, _ -> failwith "Bad construction"
+
+let aux_untensor seq i =
+  let rec aux seq acc = 
+    match seq, acc with
+    | T(x, y)::ys, 1 -> (x, y)
+    | x::ys, i when i > 1 -> aux ys (i-1)
+    | _ -> failwith "Bad construction" in
+    aux seq i
 
 let aux_perm seq sigma =
-if List.length seq <> Array.length sigma then
-  failwith "Bad construction"
-else begin
-  let m = Array.length sigma in
-  let tab_ant = Array.of_list seq in
-  let tab_img = Array.make m (A (-1)) in
+  if List.length seq <> Array.length sigma then
+    failwith "Bad construction"
+  else begin
+    let m = Array.length sigma in
+    let tab_ant = Array.of_list seq in
+    let tab_img = Array.make m (A (-1)) in
 
-  for i = 0 to m-1 do
-    let j = sigma.(i) in
-    if j < 1 || j > m then failwith "Bad construction"
-    else tab_img.(j-1) <- tab_ant.(i)
-  done;
+    for i = 0 to m-1 do
+      let j = sigma.(i) in
+      if j < 1 || j > m then failwith "Bad construction"
+      else tab_img.(j-1) <- tab_ant.(i)
+    done;
 
-  for i = 0 to m-1 do
-    if tab_img.(i) = A (-1) then failwith "Bad construction"
-  done;
+    for i = 0 to m-1 do
+      if tab_img.(i) = A (-1) then failwith "Bad construction"
+    done;
 
-  Array.to_list tab_img
-end
+    Array.to_list tab_img
+  end
 
 let rec get_seq = function
-| Atom1 i -> [A i; NA i]
-| Atom2 i -> [NA i; A i]
-| Par (i, p) -> aux_par (get_seq p) i
-| Tensor (p1, p2) -> aux_tensor (get_seq p1) (get_seq p2)
-| Permutation (sigma, p) -> aux_perm (get_seq p) sigma
+  | Atom1 i -> [A i; NA i]
+  | Atom2 i -> [NA i; A i]
+  | Par (i, p) -> aux_par (get_seq p) i
+  | Tensor (p1, p2) -> aux_tensor (get_seq p1) (get_seq p2)
+  | Permutation (sigma, p) -> aux_perm (get_seq p) sigma
 
 (*Printing*)
 let rec print_formula = function
@@ -187,27 +195,131 @@ let psi_1_inv n = function
   | (i, w) -> (i, w)
 
 let rec decode (t, s) = 
+  let sigma_build rep = 
+    let rec aux = function
+      | F((n1, []), (n2, [])) -> [n1; n2]
+      | F((n1, []), _) -> [n1]
+      | F(_, (n2, [])) -> [n2]
+      | F(_, _) -> []
+
+      | N_P((n, []), t) -> n::(aux t)
+      | N_P(_, t) -> aux t
+
+      | N_T((n, []), t1, t2) -> n::(aux t1)@(aux t2)
+      | N_T(_, t1, t2) -> (aux t1)@(aux t2) in
+    List.sort (fun x y -> x - y) (aux rep) in
+  
+  let sigma_inv sigma1 sigma2 n =
+    let m1 = Array.length sigma1 in
+    let m2 = Array.length sigma2 in
+    let inv = Array.make (m1 + m2 + 1) (-1) in
+
+    for i = 0 to m1-1 do
+      Printf.printf "sigma1 %d %d\n" (i+1) sigma1.(i);
+      if sigma1.(i) < 1 || sigma1.(i) > m1 + m2 + 1 then failwith "Bad construction1"
+      else inv.(sigma1.(i) - 1) <- i + 1
+    done;
+    
+    for i = 0 to m2-1 do
+      Printf.printf "sigma2 %d %d\n" (i+1) sigma2.(i);
+      if sigma2.(i) < 1 || sigma2.(i) > m1 + m2 + 1 then failwith "Bad construction2"
+      else inv.(sigma2.(i) - 1) <- m1 + i + 1
+    done;
+
+    for i = 0 to m1 + m2 do
+      Printf.printf "bruh %d %d\n" (i+1) inv.(i);
+      if (i + 1) <> n && inv.(i) = -1 then failwith "Bad construction3"
+    done;
+
+    inv in
+
+  let seq_split s inv n m1 =
+    let rec aux s m m1 inv gamma delta acc =
+      match s with
+      | [] when acc = m+1 -> (gamma, delta)
+      | [] -> Printf.printf "wrong acc %d\n" acc; failwith "Bad construction4"
+      | x::xs when acc = n -> aux xs m m1 inv gamma delta (acc+1)
+      | x::xs -> let k = inv.(acc-1) in
+                 if k < m1+1 then aux xs m m1 inv (x::gamma) delta (acc+1)
+                 else aux xs m m1 inv gamma (x::delta) (acc+1) in
+
+    let f1, f2 = aux_untensor s n in
+    let gamma, delta = aux s (List.length s) m1 inv [] [] 1 in
+    List.rev (f1::gamma), f2::(List.rev delta) in
+
+  let get_perm sigma1 sigma2 n = 
+    let m1, m2 = Array.length sigma1, Array.length sigma2 in
+    let perm = Array.make (m1 + m2 + 1) (-1) in
+    for i = 0 to m1 - 1 do
+      perm.(i) <- sigma1.(i)
+    done;
+
+    perm.(m1) <- n;
+
+    for i = 0 to m2 - 1 do
+      perm.(m1 + i + 1) <- sigma2.(i)
+    done;
+
+    perm in
+
+  let aux_tensor (n, w) t1 t2 s = 
+    let sigma1 = Array.of_list (sigma_build t1) in
+    let m1 = Array.length sigma1 in
+    let sigma2 = Array.of_list (sigma_build t2) in
+
+    print_string "étape0 passée\n";
+    let inv = sigma_inv sigma1 sigma2 n in
+    print_string "étape0.5 passée\n";
+
+    let sigma1_map n = function
+      | (i, Left::w) when i = n -> (m1 + 1, w)
+      | (i, w) when i <> n -> (inv.(i-1), w)
+      | _ -> failwith "Bad construction" in
+    
+    let sigma2_map n = function
+      | (i, Right::w) when i = n -> (1, w)
+      | (i, w) when i <> n -> (inv.(i-1) - m1 + 1, w)
+      | _ -> failwith "Bad construction" in
+
+    print_string "début étape1\n";
+    let s1, s2 = seq_split s inv n m1 in
+    print_string "étape1 passée\n";
+    print_seq s1; print_newline ();
+    print_seq s2; print_newline ();
+    let t1_map, t2_map = map_psi sigma1_map n t1, map_psi sigma2_map n t2 in
+    print_string "étape2 passée\n";
+    let p1, p2 = decode (t1_map, s1), decode (t2_map, s2) in
+    print_string "étape3 passée\n\n";
+    Permutation(get_perm sigma1 sigma2 n, Tensor(p1, p2)) in
+
   match t with
   | F(_, _) -> begin
     match s with
     | [A(i); NA(j)] when i = j -> Atom1(i)
     | [NA(i); A(j)] when i = j -> Atom2(i)
-    | _ -> failwith "Bad construction"
+    | _ -> print_seq s; print_newline (); failwith "Bad construction666"
     end
   | N_P((n, w), t0) -> Par(n, decode ((map_psi psi_1_inv n t0), aux_unpar s n)) 
-  | N_T(_, _, _) -> failwith "Not implemented"
+  | N_T((n, w), t1, t2) -> aux_tensor (n, w) t1 t2 s
 
 (*Main*)
-(* let proof_1 = Par(2, Permutation([|2; 1; 3; 4|], Tensor(Atom1(1), Permutation ([|3; 1; 2|], Tensor (Atom2(2), Atom2(3))))));;
+let proof_1 = Par(2, Permutation([|2; 1; 3; 4|], Tensor(Atom1(1), Permutation ([|3; 1; 2|], Tensor (Atom2(2), Atom2(3))))));;
+let proof_1_rep = encode proof_1;;
+let proof_1_back = decode proof_1_rep;;
+
+
+let proof_2 = Tensor(Atom1(1), Permutation ([|3; 1; 2|], Tensor (Atom2(2), Atom2(3))));;
+let proof_2_rep = encode proof_2;;
+let proof_2_back = decode proof_2_rep;;
 
 let () = print_proof proof_1; print_newline();;
 
-let () = print_rep (encode proof_1); print_newline();; *)
+let () = print_rep proof_1_rep; print_newline();;
 
-let proof_2 = Par(1, Permutation([|2; 1|], Atom2(1)));;
+let () = print_proof proof_1_back;;print_newline();;print_newline();;
 
 let () = print_proof proof_2; print_newline();;
 
-let () = print_rep (encode proof_2); print_newline();;
+let () = print_rep proof_2_rep; print_newline();;
 
-let () = print_proof (decode (encode proof_2));;
+let () = print_proof proof_2_back;;
